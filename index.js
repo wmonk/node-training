cb125beb8e9e590f38c3572911d90d6d9566c6e4
Will Monk <will@williammonk.co.uk>
2015-01-21 13:56:20 +0000

    Created echo server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var http = require('http');
+var qs = require('querystring');
 var logger = require('./logger');
 
 http.createServer(function (req, res) {
     logger('Request made to ' + req.url);
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('OK');
+    var toSend = qs.parse(req.url.split('?')[1]);
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(toSend));
 }).listen(3000);
