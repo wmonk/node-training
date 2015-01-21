33042031b619da57d9898568a932bef520a021b6
Will Monk <will@williammonk.co.uk>
2015-01-21 14:36:03 +0000

    Added some auth checking middleware

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('*', function (req, res) {
-    res.send(JSON.stringify(req.query));
+function isAuthenticated (req, res, next) {
+    if (req.query.auth === 'true') {
+        return next();
+    }
+
+    res.redirect('/');
+}
+
+app.get('/', function (req, res) {
+    res.send('You are not logged in!');
+});
+
+app.get('/admin', isAuthenticated, function (req, res) {
+    res.send('You are an admin!');
 });
 
 app.listen(3000);
