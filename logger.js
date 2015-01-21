56b0368d14936422e6991b6bb489d759af62ca14
Will Monk <will@williammonk.co.uk>
2015-01-21 14:58:15 +0000

    Added moment.js to logger module

--- logger.js
+++ logger.js
@@ -3,18 +3,22 @@
 var fs = require('fs');
 var path = require('path');
 var chalk = require('chalk');
+var moment = require('moment');
 
 module.exports = function loggerModule (options) {
     var pathToFile = path.join(__dirname, 'logs', 'web.txt');
     var stream = fs.createWriteStream(pathToFile);
 
     return function (req, res, next) {
-        var logMessage = Date().toString();
+        var date = moment().format('HH:mm:ss D-MM-YYYY');
+        var logMessage = date;
+        logMessage += ' -- ';
+        logMessage += req.method;
         logMessage += ' - ';
         logMessage += req.url;
 
         if (options.console) {
-            console.log(chalk.green(Date().toString()), chalk.grey('--'), chalk.red(req.method), chalk.cyan(req.url));
+            console.log(chalk.green(date), chalk.grey('--'), chalk.red(req.method), chalk.grey('-'), chalk.cyan(req.url));
         }
 
         if (options.file) {
