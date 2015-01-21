c4dd70de09e2fdda9f7033cd4059ab63af0d0b9a
Will Monk <will@williammonk.co.uk>
2015-01-21 13:50:26 +0000

    Added logging to simple server

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var http = require('http');
+var logger = require('./logger');
 
 http.createServer(function (req, res) {
+    logger('Request made to ' + req.url);
+
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('OK');
 }).listen(3000);
