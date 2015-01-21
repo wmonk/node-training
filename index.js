c023160f36f11ec563cc444e1426978643683799
Will Monk <will@williammonk.co.uk>
2015-01-21 14:32:47 +0000

    Added logger back in

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 'use strict';
 
 var express = require('express');
+var logger = require('./logger');
 
 var app = express();
+
+app.use(function (req, res, next) {
+    logger(req.url);
+
+    next();
+});
 
 app.get('*', function (req, res) {
     res.send(JSON.stringify(req.query));
