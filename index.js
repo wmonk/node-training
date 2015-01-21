c6000f07ef7038f4673ac13934ed616b707f29ef
Will Monk <will@williammonk.co.uk>
2015-01-21 13:45:29 +0000

    Added logger module

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log('Hello World!');
+var logger = require('./logger');
+
+logger('Hello World!');
---
+++ logger.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function loggerModule (toLog) {
+    console.log(toLog);
+};
